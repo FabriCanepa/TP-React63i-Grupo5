353768380f3c0a5a1788c4cab197faa3f50a49ac
Fabricio Canepa <fabri.caspel@gmail.com>
2023-09-29 20:37:28 -0300

    termino ejercicio 3

--- fabricio-react/src/main.jsx
+++ fabricio-react/src/main.jsx
@@ -5,14 +5,16 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import './index.css'
 
-import Ejercicio1 from './components/Ejercicios/Ejercicio1.jsx'
-import Ejercicio2 from './components/Ejercicios/Ejercicio2.jsx'
+// import Ejercicio1 from './components/Ejercicios/Ejercicio1.jsx'
+// import Ejercicio2 from './components/Ejercicios/Ejercicio2.jsx'
+import Ejercicio3 from './components/Ejercicios/Ejercicio3.jsx'
 
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-<Ejercicio1/>
-<Ejercicio2 myFriend= 'My Friend'/>
+{/* <Ejercicio1/> */}
+{/* <Ejercicio2 myFriend= 'My Friend'/> */}
+<Ejercicio3/>
   </React.StrictMode>,
 )
